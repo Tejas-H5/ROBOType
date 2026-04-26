fdf8f98e1209c9eb4988887e0b13791aeaeeb24f
Tejas Hegde <45031546+Tejas-H5@users.noreply.github.com>
2026-04-26 11:34:27 +0930

    ok build script

--- Readme.md
+++ Readme.md
@@ -1,12 +1,14 @@
 # RoboType
 
-This was my submission to the 2026 handmade jam. 
-I wasn't actually going to enter that jam - I only thought of this idea ~3 days before it ended, but it ended up
-being a pretty good one, so I've tried to finish it.
+This is the completed version of my submission to the 2026 handmade jam. 
 
 ## What is it
 
-It's a typing harness like MonkeyType. Except - you can use Ctrl+W to backspace the last word, among other things.
+It's a typing harness like MonkeyType. 
+Except - you can use Ctrl+W to backspace the last word, among other things.
+Due to various technical reasons, Ctrl+W will always close the current tab on any web page. 
+As such, this program needs to be native.
+
 The input field actually behaves a lot like a normal text input that you might use in other places on your computer.
 Try to get the fastest time on every sample. 
 Make no mistakes. \[perfect\] !
---
+++ build_windows.bat
@@ -0,0 +1,33 @@
+@echo off
+
+SET VERSION=v1.0.0
+
+SET OUTPUT_DIR=build\windows\output
+SET BUILDS_DIR=build\windows\
+
+rmdir /s /q %OUTPUT_DIR%
+md %OUTPUT_DIR%
+if %errorlevel% neq 0 (
+    echo Task failed with error %errorlevel%
+    exit /b %errorlevel%
+)
+
+odin build . -o:speed -out:%OUTPUT_DIR%\ROBOType.exe
+if %errorlevel% neq 0 (
+    echo Task failed with error %errorlevel%
+    exit /b %errorlevel%
+)
+
+md %OUTPUT_DIR%\collections
+robocopy .\collections .\%OUTPUT_DIR%\collections /s /e
+REM https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy#exit-return-codes
+if %errorlevel% GEQ 8 (
+    echo Task failed with error %errorlevel%
+    exit /b %errorlevel%
+)
+
+powershell -C "Compress-Archive -Path %OUTPUT_DIR% -DestinationPath %BUILDS_DIR%\ROBOType-windows-%VERSION%.zip"
+if %errorlevel% neq 0 (
+    echo Task failed with error %errorlevel%
+    exit /b %errorlevel%
+)
